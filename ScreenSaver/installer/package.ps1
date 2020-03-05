f80dee8aaefe5e2c8a59e6648312289f70840ffe
Kraka <noteb5@gmail.com>
2020-03-04 23:22:30 -0700

    Release

--- ScreenSaver/installer/package.ps1
+++
@@ -1,5 +0,0 @@
-#powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('.\bin\all', '..\'); }"
-Copy-Item 'Aerial.scr' '.\Package\Aerial.scr';
-Copy-Item 'eerial.scr' '.\Package\Aerial.scr';
-Add-Type -A 'System.IO.Compression.FileSystem';
-[IO.Compression.ZipFile]::CreateFromDirectory('.\bin\all', '..\');
